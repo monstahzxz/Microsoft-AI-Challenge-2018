79be9ffac4bf02dbe488c12bb55d9a95db133723
m0nstah <saranprasad97@gmail.com>
2019-08-03 22:47:52 +0530

    source only

---
+++ src/bi_eval.py
@@ -0,0 +1,249 @@
+import numpy as np
+import tensorflow as tf
+import math
+
+
+
+'''def model(qX, pX, lX):
+	query_X = tf.placeholder(tf.float32, [None, timesteps_Q, embed_dims])
+	passage_X = tf.placeholder(tf.float32, [None, timesteps_P, embed_dims])
+	labels = tf.placeholder(tf.float32, [None])
+	Q_input = tf.unstack(query_X, timesteps_Q, 1)
+	P_input = tf.unstack(passage_X, timesteps_P, 1)
+	W1 = tf.get_variable("W1", [1, timesteps_P], initializer = tf.contrib.layers.xavier_initializer())
+	b1 = tf.get_variable("b1", [timesteps_Q], initializer = tf.contrib.layers.xavier_initializer())
+	W2 = tf.get_variable("W2", [timesteps_Q, 1], initializer = tf.contrib.layers.xavier_initializer())
+	b2 = tf.get_variable("b2", [1], initializer = tf.contrib.layers.xavier_initializer())
+	
+	lstm_layer_Q = tf.contrib.rnn.LSTMCell(num_units)
+	with tf.variable_scope('query'):
+		outputs_Q, _ = tf.nn.static_rnn(lstm_layer_Q, Q_input, dtype=tf.float32) #Shape = (timesteps_Q, num_examples, num_units)
+	lstm_layer_P = tf.contrib.rnn.LSTMCell(num_units)
+	with tf.variable_scope('passage'):
+		outputs_P, _ = tf.nn.static_rnn(lstm_layer_P, P_input, dtype=tf.float32) #Shape = (timesteps_P, num_examples, num_units)
+
+	S = tf.math.softmax(tf.tensordot(outputs_P, tf.transpose(outputs_Q), axes=[[1,2],[0,1]]), axis=1)
+	Z = tf.add(tf.matmul(W1, S), b1)
+	A = tf.math.sigmoid(tf.add(tf.matmul(Z, W2), b2))
+
+	cost = tf.pow(tf.subtract(A, labels), 2)
+	optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+	init = tf.global_variables_initializer()
+	saver = tf.train.Saver()
+	sess = tf.Session()
+	
+	
+	sess.run(init)
+
+	for i in range(epochs):
+		for j in range(int(num_examples / minibatch_size) + 1):
+			#print(j)
+			que = qX[minibatch_size * j : minibatch_size * (j + 1)]
+			pas = pX[minibatch_size * j : minibatch_size * (j + 1)]
+			lab = lX[minibatch_size * j : minibatch_size * (j + 1)]
+			_, e_cost = sess.run([optimiser, cost], feed_dict = {query_X : que, passage_X : pas, labels : lab})
+			if (j + 1) % 100 == 0:
+				print('Cost of batch ' + str(j + 1))
+				print(e_cost)
+				saver.save(sess, "D:/NNs/RNN/Word embeddings/work/models/model.ckpt")
+
+	save_path = saver.save(sess, "D:/NNs/RNN/Word embeddings/work/models/model.ckpt")
+	print("Model saved in path: %s" % save_path)'''
+
+def shuffle(X):
+	#np.random.shuffle(X)
+	X = np.array(X)
+
+	query_X = [X[:,0]]
+	passage_X = [X[:,1]]
+	#labels_X = [X[:,2]]
+
+	query_X = query_X[0][:].tolist()
+	passage_X = passage_X[0][:].tolist()
+	#labels_X = labels_X[0][:].tolist()
+
+	return query_X, passage_X
+
+
+def model(data_file, result_file):
+	file = open(data_file, 'r', encoding='utf-8')
+	out_file = open(result_file, 'w', encoding='utf-8')
+	#query_X = []
+	#passage_X = []
+	#labels_X = []
+	X = []
+	preds = []
+	i = 0
+	l = 0
+
+	query_X = tf.placeholder(tf.float32, [None, timesteps_Q, embed_dims])
+	passage_X = tf.placeholder(tf.float32, [None, timesteps_P, embed_dims])
+	labels = tf.placeholder(tf.float32, [None])
+	Q_input = tf.unstack(query_X, timesteps_Q, 1)
+	P_input = tf.unstack(passage_X, timesteps_P, 1)
+	WA = tf.get_variable("WA", [4 * num_units, 2 * num_units], initializer = tf.contrib.layers.xavier_initializer())
+	bA = tf.get_variable("bA", [2 * num_units], initializer = tf.contrib.layers.xavier_initializer())
+	W1 = tf.get_variable("W1", [timesteps_P * 2 * num_units, hidden_size_1], initializer = tf.contrib.layers.xavier_initializer())
+	b1 = tf.get_variable("b1", [hidden_size_1], initializer = tf.contrib.layers.xavier_initializer())
+	W2 = tf.get_variable("W2", [hidden_size_1, hidden_size_2], initializer = tf.contrib.layers.xavier_initializer())
+	b2 = tf.get_variable("b2", [hidden_size_2], initializer = tf.contrib.layers.xavier_initializer())
+	W3 = tf.get_variable("W3", [hidden_size_2, hidden_size_3], initializer = tf.contrib.layers.xavier_initializer())
+	b3 = tf.get_variable("b3", [hidden_size_3], initializer = tf.contrib.layers.xavier_initializer())
+	W4 = tf.get_variable("W4", [hidden_size_3, 1], initializer = tf.contrib.layers.xavier_initializer())
+	b4 = tf.get_variable("b4", [1], initializer = tf.contrib.layers.xavier_initializer())
+
+	lstm_layer_Q = tf.contrib.rnn.LSTMCell(num_units)
+	with tf.variable_scope('query'):
+		outputs_Q, _, _ = tf.nn.static_bidirectional_rnn(lstm_layer_Q, lstm_layer_Q, Q_input, dtype=tf.float32) #Shape = (timesteps_Q, num_examples, num_units)
+	lstm_layer_P = tf.contrib.rnn.LSTMCell(num_units)
+	with tf.variable_scope('passage'):
+		outputs_P, _, _ = tf.nn.static_bidirectional_rnn(lstm_layer_P, lstm_layer_P, P_input, dtype=tf.float32) #Shape = (timesteps_P, num_examples, num_units)
+
+	outputs_P_in = tf.transpose(outputs_P, perm=[1,0,2])#tf.reshape(tf.convert_to_tensor(outputs_P),[-1,timesteps_P,2 * num_units])
+	outputs_Q_in = tf.transpose(outputs_Q, perm=[1,2,0])#tf.reshape(tf.convert_to_tensor(outputs_Q),[-1,2 * num_units,timesteps_Q])
+	S = tf.math.softmax(tf.matmul(outputs_P_in, outputs_Q_in), axis=2)
+	C = tf.matmul(S, tf.transpose(outputs_Q, perm=[1,0,2]))
+	A_in = tf.concat((C,outputs_P_in),axis=2)
+	A = tf.add(tf.tensordot(A_in, WA,[[2],[0]]), bA)
+	A_out = tf.reshape(A,[-1,timesteps_P * 2 * num_units])
+	#S = tf.matmul(outputs_P_in, outputs_Q_in)
+	#S_r = tf.reshape(S,[-1, timesteps_Q * timesteps_P])
+	#S = tf.reshape(S,[-1,timesteps_P,timesteps_Q])
+	Z1 = tf.add(tf.matmul(A_out, W1), b1)
+	A1 = tf.nn.relu(Z1)
+	Z2 = tf.add(tf.matmul(A1, W2), b2)
+	A2 = tf.nn.relu(Z2)
+	Z3 = tf.add(tf.matmul(A2, W3), b3)
+	A3 = tf.nn.relu(Z3)
+	Z4 = tf.reshape(tf.add(tf.matmul(A3, W4), b4),[-1])
+	#Z = tf.reshape(Z,[-1,1,40])
+	#A = tf.math.sigmoid(tf.add(tf.tensordot(Z, W2, axes=[[2],[0]]), b2))
+	#A = tf.reshape(A,[-1])
+
+	cost = tf.math.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels,logits=Z4))
+	optimiser = tf.train.AdamOptimizer().minimize(cost)
+
+	#init = tf.global_variables_initializer()
+	saver = tf.train.Saver()
+	sess = tf.Session()
+
+	#sess.run(init)
+	saver.restore(sess,'D:/NNs/RNN/Word embeddings/work/models14/model.ckpt')
+
+	for example in file:
+		'''if l < 280000:
+			print('skipping ' + str(l + 1))
+			l = l + 1
+			continue'''
+
+		if (l + 1) % 1000 == 0:
+			print('Test ' + str(l + 1) + ' done')
+
+		tokens = example.strip().lower().split('\t')
+
+		X.append([])
+		query_id = tokens[0]
+		query_vec = tokens[1]
+		passage_vec = tokens[2]
+		passage_id = tokens[3]
+
+		query_id = int(query_id)
+		query_vec = [float(x) for x in query_vec.split()]
+		passage_vec = [float(x) for x in passage_vec.split()]
+		passage_id = float(passage_id)
+
+		query_vec = np.array(query_vec).reshape(-1,embed_dims)
+		query_vec = np.append(query_vec, np.zeros((timesteps_Q - query_vec.shape[0],50)),axis=0)
+		passage_vec = np.array(passage_vec).reshape(-1,embed_dims)
+		passage_vec = np.append(passage_vec, np.zeros((timesteps_P - passage_vec.shape[0],50)),axis=0)
+		#label = np.array(label)
+
+		'''if label == 1:
+			for z in range(16):
+				X[i].append(query_vec)
+				X[i].append(passage_vec)
+				X[i].append(label)
+				X.append([])
+				i = i + 1	'''
+			
+		
+		X[0].append(query_vec)
+		X[0].append(passage_vec)
+		#X[i].append(label)
+		#query_X.append(query_vec)
+		#passage_X.append(passage_vec)
+		#labels_X.append(label)
+
+		#if (i + 1) % 18721 == 0:
+		#X = np.array(X)
+		qX, pX = shuffle(X)
+		#print('Example ' + str(l + 1) + ' read')		
+		#for k in range(epochs):
+		#for j in range(int(18721 / minibatch_size) + 1):
+		#print(j)
+		que = qX
+		pas = pX
+		#lab = lX
+		#lab = np.array(lab).reshape(len(lab),1)
+		pred = sess.run([Z4], feed_dict = {query_X : que, passage_X : pas})
+		#e_A = sess.run(A2, feed_dict = {query_X : que, passage_X : pas, labels : lab})
+		#if (j + 1) % 70 == 0:
+			#print('Example : ' + str(l + 1) + '.. Cost of epoch ' + str(k + 1) + ' batch ' + str(j + 1))
+		#print(pred[0])
+		#preds.append(1 / ( 1 + math.exp(-pred[0])))
+		preds.append(1 - 1 / (1 + math.exp(pred[0])))
+			#print(e_A)
+			#print(lab)
+			#print(e_P)
+			#print(e_Q)
+			#print(lab)
+			#saver.save(sess, "D:/NNs/RNN/Word embeddings/work/models2/model.ckpt")
+		
+		#saver.save(sess, "D:/NNs/RNN/Word embeddings/work/models2/model.ckpt")			
+		if i == 9:
+			out_file.write(str(query_id))
+			for o in range(10):
+				out_file.write('\t' + str(preds[o]))
+			out_file.write('\n')
+			preds = []
+			i = -1
+
+		X = []
+		i = i + 1	
+		l = l + 1	
+
+	#query_X = np.array(query_X)
+	#passage_X = np.array(passage_X)
+	#labels_X = np.array(labels_X)
+
+
+
+if __name__ == '__main__':
+	
+	learning_rate = 0.25
+	epochs = 20
+	num_units = 64
+	num_examples = 5241880
+	embed_dims = 50
+	num_epochs = 10
+	minibatch_size = 128
+	timesteps_Q = 40
+	timesteps_P = 300
+	hidden_size_1 = 1024
+	hidden_size_2 = 516
+	hidden_size_3 = 256
+
+
+	data_file = 'evaluate.tsv'
+	evaluation_file = 'eval2_unlabelled_embedded.tsv'
+	result_file = 'answer.tsv'
+	
+
+	X = model(evaluation_file, result_file)
+	#print('\nTrained\n')
+
+	#query_X, passage_X, labels_X = shuffle(X)
+
+	#model(query_X, passage_X, labels_X)
+	#evaluate(evaluation_file, result_file)
---
+++ src/pre.py
@@ -0,0 +1,76 @@
+import re
+
+'''i = 1
+
+f = open('data.tsv',"r",encoding="utf-8")
+fw = open('corpus.tsv',"w",encoding="utf-8")
+for line in f:
+	tokens = line.strip().lower().split("\t")
+	query_id,query,passage,label = tokens[0],tokens[1],tokens[2],tokens[3]
+	#fw.write(passage+"\n")
+	if i % 2455 == 0:
+		break
+	i = i + 1
+		
+f.close()
+fw.close()
+print(tokens)'''
+
+def embedIt(embedFile):
+	file = open(embedFile,'r',encoding='utf-8',errors='ignore')
+	for line in file:
+		tokens = line.strip().split()
+		word = tokens[0]
+		vec = tokens[1:]
+		vec = " ".join(vec)
+		gloveEmbeds[word] = vec
+	gloveEmbeds['zerovec'] = '0.0 ' * embedDim
+	file.close()
+
+
+def convertIntoEmbed(inputFile, outputFile):
+	#tokenise and convert words in input file to word vectors and load into output file
+	inFile = open(inputFile,'r',encoding='utf-8',errors='ignore')
+	outFile = open(outputFile,'w',encoding='utf-8')
+
+	i = 0
+	for line in inFile:
+		tokens = line.strip().lower().split('\t')
+		queryId, query, passage, label, passageId = tokens[0], tokens[1], tokens[2], tokens[3], tokens[4]
+		#Convert queries and passages to vecs.. (download glove6b and extract to glove6b.txt)
+		words = re.split('\W',query)
+		words = [notEmptyWord for notEmptyWord in words if notEmptyWord]
+		queryVec  = ''
+		for word in words:
+			if word in gloveEmbeds:
+				queryVec += gloveEmbeds[word] + ' '
+			else:
+				queryVec += gloveEmbeds['zerovec']	+ ' '
+
+		words = re.split('\W',passage)
+		words = [notEmptyWord for notEmptyWord in words if notEmptyWord]
+		passageVec = ''	
+		for word in words:
+			if word in gloveEmbeds:
+				passageVec += gloveEmbeds[word] + ' '
+			else:
+				passageVec += gloveEmbeds['zerovec']	+ ' '
+
+		outFile.write(queryVec + '\t' + passageVec + '\t' + label + '\n')
+		#print(queryVec + '\t' + passageVec + '\t' + label + '\n')
+		if (i + 1) % 10000 == 0:
+			print('Example ' + str(i + 1) + ' done')
+		i = i + 1
+		
+		#print(re.split('\W',query))
+		#print(query.split(' '))
+		#break
+
+
+if __name__ == '__main__':
+	embedFile = 'glove.6B.50d.txt'
+	gloveEmbeds = {}
+	embedDim = 50
+	gloveEmbeds['zerovec'] = '0.0 ' * embedDim
+	embedIt(embedFile)	#enter glove embed txt file'''
+	convertIntoEmbed('data.tsv','evaluate.tsv') #evaluate.tsv contains queryId, queryVec, passageVec, label, passageId
